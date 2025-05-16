5e7f333fe790e1ef3533b964d15fc9ef3f7513f7
Samuel Souza <silva.samueldev@gmail.com>
2025-05-16 09:59:16 -0300

    :wrench: melhorando implementação

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,26 @@ import Arvore.*;
 public class Main {
     public static void main(String[] args) {
         Arvore arvore = new Arvore();
-        No noRaiz = new No('A');
-        No noEsquerdo = new No('B');
-        No noDireito = new No('C');
-        noRaiz.setLeft(noEsquerdo);
-        noRaiz.setRight(noDireito);
-        arvore.setRaiz(noRaiz);
+        No NoA = new No('A');
+        No NoB = new No('B');
+        No NoC = new No('C');
+        No NoD = new No('D');
+        No NoE = new No('E');
+        No NoF = new No('F');
+
+        arvore.setRaiz(NoA);
+        arvore.getRaiz().setLeft(NoB);
+        arvore.getRaiz().setRight(NoC);
+        arvore.getRaiz().getLeft().setLeft(NoD);
+        arvore.getRaiz().getLeft().setRight(NoE);
+        arvore.getRaiz().getRight().setRight(NoF);
+
 
         System.out.println(arvore.getRaiz().getConteudo());
         System.out.println(arvore.getRaiz().getLeft().getConteudo());
         System.out.println(arvore.getRaiz().getRight().getConteudo());
+        System.out.println(arvore.getRaiz().getLeft().getLeft().getConteudo());
+        System.out.println(arvore.getRaiz().getLeft().getRight().getConteudo());
+        System.out.println(arvore.getRaiz().getRight().getRight().getConteudo());
     }
 }
